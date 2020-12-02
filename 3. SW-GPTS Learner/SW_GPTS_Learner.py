89a13bd2cc5657ab6571826b290e059edf42cc4c
GabrieleGavarini <gabrielegavarini@gmail.com>
2020-12-02 16:34:32 +0100

    - Added normalization in ch. 3 - Moved computation of ideal arm outside of main loop in ch. 3

--- 3. SW-GPTS Learner/SW_GPTS_Learner.py
+++ 3. SW-GPTS Learner/SW_GPTS_Learner.py
@@ -2,27 +2,32 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 from sklearn.gaussian_process import GaussianProcessRegressor
-from sklearn.gaussian_process.kernels import RBF, ConstantKernel as C
+from sklearn.gaussian_process.kernels import Matern, WhiteKernel, RBF, ConstantKernel as C
 
 
 class SW_GPTS_Learner:
-    
+
     def __init__(self, arms, sigma=5, window_length=0):
         self.arms = arms
         self.n_arms = len(arms)
         self.means = np.zeros(self.n_arms)
-        self.sigmas = np.ones(self.n_arms)*sigma
+        self.sigmas = np.ones(self.n_arms) * sigma
 
         self.pulled_arms = np.array([])
         self.collected_rewards = np.array([])
 
         self.window_length = window_length
 
-        alpha = 10.0
-        kernel = C(1.0, (1e-5, 1e5)) * RBF(1.0, (1e-5, 1e5))
+        alpha = 1.5
+
+        theta = 1
+        l = 1
+
+        kernel = C(theta, (1e-5, 1e5)) * RBF(l, (1e-10, 1e10))
+
         self.gp = GaussianProcessRegressor(kernel=kernel,
-                                           alpha=alpha**2,
-                                           normalize_y=False,
+                                           alpha=alpha ** 2,
+                                           normalize_y=True,
                                            n_restarts_optimizer=10)
 
     def sample_values(self):
@@ -42,7 +47,7 @@ class SW_GPTS_Learner:
         arm_index (int): the index of the arm that has been pulled
         reward (float): the reward received by pulling the arm
         """
-        self.pulled_arms = np.append(self.pulled_arms, self.arms[arm_index])
+        self.pulled_arms = np.append(self.pulled_arms, arm_index)
         self.collected_rewards = np.append(self.collected_rewards, reward)
 
         if len(self.pulled_arms) > self.window_length:
@@ -56,8 +61,11 @@ class SW_GPTS_Learner:
         x = np.atleast_2d(self.pulled_arms).T
         y = self.collected_rewards
 
+        if len(y) == 1:
+            return
+
         self.gp.fit(x, y)
-        self.means, self.sigmas = self.gp.predict(np.atleast_2d(self.arms).T, return_std=True)
+        self.means = self.gp.predict(np.array(list(np.ndindex(len(self.arms)))))
         self.sigmas = np.maximum(self.sigmas, 1e-2)
 
     def update(self, arm_index, reward):
@@ -71,24 +79,24 @@ class SW_GPTS_Learner:
         """
         self.update_observations(arm_index, reward)
         self.update_model()
-        
+
     def pull_arm(self):
         """
         Choose which arm to pull based on the result of a sampling of the gaussian process
-        
+
         Returns:
         (int): the index of the arm that maximizes the observation
         """
         sampled_values = np.random.normal(self.means, self.sigmas)
         return np.argmax(sampled_values)
-    
+
     # FOR TESTING PURPOSES
     def plot(self, ax, function):
         ax.plot(self.arms, function(self.arms))
-        
+
         ax.plot(self.pulled_arms, self.collected_rewards, 'ro')
-        
+
         ax.plot(self.arms, self.means)
-        
+
     def predict(self, point):
         return self.gp.predict([[point]])
--- 3. SW-GPTS Learner/experiment.py
+++ 3. SW-GPTS Learner/experiment.py
@@ -7,8 +7,27 @@ import math
 import numpy as np
 from matplotlib import pyplot as plt
 
+
+def compute_ideal(scenarios):
+    # Compute the real value of the tru function
+    real_values = []
+    for scenario in scenarios:
+        real_values.append(scenario.phase_values[:number_of_budgets])
+
+    # FIND IDEAL ARMS (to compute regret)
+    # Given the real value of the true function, find the ideal combination of arms
+    arms = ideal_optimizer.optimize(real_values)
+
+    # Compute the ideal optimal result
+    result = 0
+    for index, scenario in enumerate(scenarios):
+        result += scenario.phase_values[arms[index]]
+
+    return arms, result
+
+
 number_of_experiments = 5
-window_coefficients = [1, 2, 3, 0]
+window_coefficients = [1, 3, 5, 0]
 
 # The budget cap and the possible daily budget that can be set
 budget_cap = 27000
@@ -17,7 +36,7 @@ daily_budgets = np.linspace(5000, budget_cap, endpoint=True, num=45).astype(int)
 # The number of possible budgets that can be allocated to each subcampaign
 number_of_budgets = 11
 
-time_horizon = 300
+time_horizon = 150
 phase_duration = int(time_horizon / 3)
 
 regret = []
@@ -33,7 +52,7 @@ for i in range(0, len(window_coefficients)):
     for e in range(0, number_of_experiments):
 
         print('\n')
-        print('Starting experiment', e + 1)
+        print('Starting experiment', e + 1, '(c=', window_coefficients[i], ')')
 
         scenarios = [ThreePhasesScenario(daily_budgets=daily_budgets,
                                          campaign=0,
@@ -59,29 +78,24 @@ for i in range(0, len(window_coefficients)):
         optimal_rewards_per_round = np.zeros(time_horizon)
         ideal_rewards_per_round = np.zeros(time_horizon)
 
+        ideal_arms, ideal_result = compute_ideal(scenarios)
+
         for t in tqdm(range(0, time_horizon)):
+
+            # The ideal result obtainable at time t
+            if t % phase_duration == 1:
+                ideal_arms, ideal_result = compute_ideal(scenarios)
 
             # Sample the Gaussian Process for each subcampaign
             sampled_values = []
             for subcampaign in subcampaigns:
                 sampled_values.append(subcampaign.sample_values())
 
-            # Compute the real value of the tru function
-            real_values = []
-            for scenario in scenarios:
-                real_values.append(scenario.phase_values[:number_of_budgets])
-
             # FIND OPTIMAL ARMS
             # Find the best combination of arms to play at time t based on the sampling of the Gaussian Processes
             optimal_arms = optimizer.optimize(sampled_values)
             # The optimal result obtainable at time t by playing the optimal arms
             optimal_result = 0
-
-            # FIND IDEAL ARMS (to compute regret)
-            # Given the real value of the true function, find the ideal combination of arms
-            ideal_arms = ideal_optimizer.optimize(real_values)
-            # The ideal result obtainable at time t
-            ideal_result = 0
 
             for sub_index in range(0, len(subcampaigns)):
                 # Sample the true function to get a reward for the played arm
@@ -91,15 +105,13 @@ for i in range(0, len(window_coefficients)):
                 # Update the model based on the result of the played arm
                 subcampaigns[sub_index].update(optimal_arms[sub_index], reward)
 
-                # Compute the ideal optimal result
-                ideal_result += scenarios[sub_index].phase_values[ideal_arms[sub_index]]
-
                 # Advance the time of the scenario and eventually trigger a new phase
                 scenarios[sub_index].advance_time()
 
             optimal_rewards_per_round[t] = optimal_result
             ideal_rewards_per_round[t] = ideal_result
-            # print("[", t, "]Optimal solution:", optimal_arms, " (", ideal_arms, ")",
+            # print("\n",
+            #       "[", t, "]Optimal solution:", optimal_arms, "(", ideal_arms, ")",
             #       "with a result of: ", round(optimal_result, 2), " (", round(ideal_result, 2), ").")
 
         regret.append(ideal_rewards_per_round - optimal_rewards_per_round)
@@ -117,8 +129,8 @@ for i in range(0, len(window_coefficients)):
     ax[int(i/2), i % 2].set_ylabel("Regret", fontsize=14)
 
     ax[int(i/2), i % 2].tick_params(length=0)
-    ax[int(i/2), i % 2].set_yticks(np.linspace(0, 30000, 11))
-    ax[int(i/2), i % 2].set_yticklabels(np.linspace(0, 30000, 11).astype(np.int64), fontsize=12, alpha=0.7)
+    ax[int(i/2), i % 2].set_yticks(np.linspace(0, 50000, 11))
+    ax[int(i/2), i % 2].set_yticklabels(np.linspace(0, 50000, 11).astype(np.int64), fontsize=12, alpha=0.7)
 
     ax[int(i/2), i % 2].spines['right'].set_alpha(0)
     ax[int(i/2), i % 2].spines['left'].set_alpha(0.3)
